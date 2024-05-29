507833b3bebc21211276f987552d8246ce4b1579
Maycon <mayconisac026@gmail.com>
2024-05-28 21:54:14 -0300

    Estruturando  Arrays

---
+++ dia 10/dia10.js
@@ -0,0 +1,18 @@
+const array = []
+const indices = []
+let contadorIndice = 0
+
+const numeroProcurado = parseInt(prompt("Digite o número a ser procurado:"));
+
+for( let i = 0; i < 5; i++){
+    const elemento = parseInt(prompt(`Digite o elemento ${i + 1}:`))
+    array[i] = elemento;
+}
+
+for(let i = 0; i < 5; i++){
+    if (array[i] === numeroProcurado) {
+    indices[contadorIndice] = i
+    contadorIndice++
+    }
+}
+console.log(`O número ${numeroProcurado} foi encontrado nos índices: ${indices}`);
