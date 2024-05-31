140028b22276fa723d5405dec38cd2c7681cf865
Maycon <mayconisac026@gmail.com>
2024-05-30 23:27:37 -0300

    adicioando alert

--- dia 5/dia5.js
+++ dia 5/dia5.js
@@ -1,18 +1,21 @@
-let num1 = parseInt (prompt("Digite um número"))
-let num2 = parseInt(prompt("Digite o segundo número"))
-let opcao = parseInt(prompt("Escolha a operação que deseja realizar:" + "\n1 = +; \n2 = -; \n3 = *; \n4 = /"))
+let num1 = Number(prompt("Digite um número"))
+let num2 = Number(prompt("Digite o segundo número"))
+let opcao = parseInt(("Digite o os seguintes números com as seguintes operações \n1= + \n2 = - \n3 = * \n4 = /"))
 
 switch (opcao) {
     case 1:
         console.log(num1 + " + " + num2 + " = " + (num1 + num2))
         break;
     case 2:
-        console.log(num1 + " + " + num2 + " = " + (num1 - num2))
+        console.log(num1 + " - " + num2 + " = " + (num1 - num2))
         break;
     case 3:
         console.log(num1 + " * " + num2 + " = " + (num1 * num2))
         break;
-        default:
-            console.log(num1 + " / " + num2 + " = " + (num1 / num2))
-            break;
+    case 4:
+        console.log(num1 + " / " + num2 + " = " + (num1 / num2))
+        break;
+    default:
+         alert("Operação Inválida!")
+        break;
 }
